42772eed77cbb19702ffc009f86d58a07af94aec
Gabriel Rezende Gomes <gabriel.rgomes@al.infnet.edu.br>
2026-05-07 14:27:28 -0300

    feat(social): cria a entidade GrupoEntity

--- src/main/java/com/apportion/apportion/Social/Model/Entidades/GrupoEntity.java
+++ src/main/java/com/apportion/apportion/Social/Model/Entidades/GrupoEntity.java
@@ -1,4 +1,31 @@
-package com.apportion.apportion.Social.Model;
+package com.apportion.apportion.Social.Model.Entidades;
 
-public class GrupoEntity {
+import com.apportion.apportion.Identity.Model.Entidades.UsuarioEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "grupos")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class GrupoEntity
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "NomeDoGrupo", nullable = false)
+    private String nomeDoGrupo;
+    @ManyToMany(mappedBy = "grupo")
+    private Set<UsuarioEntity> usuarios = new HashSet<>();
+    @OneToMany(mappedBy = "grupo",cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<ViagemEntity> viagens = new HashSet<>();
+
 }
