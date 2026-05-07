53075627e8ff17e92ee80f0cd861c837c3527dc5
Gabriel Rezende Gomes <gabriel.rgomes@al.infnet.edu.br>
2026-05-07 14:27:51 -0300

    feat(social): cria a entidade ViagemEntity

--- src/main/java/com/apportion/apportion/Social/Model/Entidades/ViagemEntity.java
+++ src/main/java/com/apportion/apportion/Social/Model/Entidades/ViagemEntity.java
@@ -1,4 +1,26 @@
 package com.apportion.apportion.Social.Model.Entidades;
 
-public class ViagemEntity {
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "viagem")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ViagemEntity
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    @ManyToOne
+    @JoinColumn(name = "grupo_id", nullable = false)
+    private GrupoEntity grupo;
+
 }
