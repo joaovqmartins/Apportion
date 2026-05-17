acad015d78e3b65210b813f98bbb5c4df3a29247
joaovqmartins <joavmartin3zp1n@gmail.com>
2026-05-17 17:10:19 -0300

    fix(social): ajusta GrupoResponseDto e atualiza testes do grupo

--- src/main/java/com/apportion/apportion/Social/Model/Entidades/Dto/Responses/GrupoResponseDto.java
+++ src/main/java/com/apportion/apportion/Social/Model/Entidades/Dto/Responses/GrupoResponseDto.java
@@ -1,6 +1,10 @@
 package com.apportion.apportion.Social.Model.Entidades.Dto.Responses;
 
+import com.apportion.apportion.Identity.Model.Entidades.Dto.Responses.UserResponseDto;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
+
+import java.util.Set;
 
 @Getter
 @Setter
@@ -10,4 +14,7 @@ import lombok.*;
 public class GrupoResponseDto {
     private Long id;
     private String nomeDoGrupo;
+
+    @JsonIgnoreProperties("grupos")
+    private Set<UserResponseDto> usuarios;
 }
