157d1d2c0dfddb361f6861e1b5f2071846439fdb
gabriel <gabriel.rgomes@al.infnet.edu.br>
2026-05-18 19:45:16 -0300

    feat:criacao do enum de roles, e comeco da implementacao com spring security

--- src/main/java/com/apportion/apportion/Identity/Model/Entidades/UsuarioEntity.java
+++ src/main/java/com/apportion/apportion/Identity/Model/Entidades/UsuarioEntity.java
@@ -1,12 +1,21 @@
 package com.apportion.apportion.Identity.Model.Entidades;
 
+import com.apportion.apportion.Identity.Model.Enums.Roles;
 import jakarta.persistence.*;
 import lombok.*;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
+import java.util.Collection;
 import java.util.Date;
+import java.util.List;
+
+import static com.apportion.apportion.Identity.Model.Enums.Roles.ADMIN;
 
 @Entity
 @Table(name = "Usuario")
@@ -15,7 +24,7 @@ import java.util.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class UsuarioEntity {
+public class UsuarioEntity implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -36,6 +45,25 @@ public class UsuarioEntity {
     @Column(nullable = false)
     private OffsetDateTime dataCriacao;
 
+    @Column(nullable = false)
+    private Roles role;
+
     @PrePersist
     public void OnCrate(){this.dataCriacao = OffsetDateTime.now();}
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if(this.role == ADMIN) return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"),new SimpleGrantedAuthority("ROLE_USER"));
+        else return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
 }
